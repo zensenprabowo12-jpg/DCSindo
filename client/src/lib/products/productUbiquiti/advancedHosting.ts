d887e1176e520e0a2fc5c0ccf430a99f1e13e397
zensen.prabowo <zensenprabowo12@gmail.com>
2026-04-23 09:46:46 +0700

    update

--- client/src/lib/products/productUbiquiti/advancedHosting.ts
+++ client/src/lib/products/productUbiquiti/advancedHosting.ts
@@ -1361,14 +1361,6 @@ export const advancedHostingProducts: Product[] = [
       },
     ],
 
-    // Item yang ada dalam box produk (gambar bisa diganti)
-    inTheBox: [
-      { name: "Device", image: "/images/dcs-box.png" },
-      { name: "Mounting Kit", image: "/images/dcs-box.png" },
-      { name: "Power Cable", image: "/images/dcs-box.png" },
-      { name: "Quick Start", image: "/images/dcs-box.png" }
-    ],
-
     // Addon/aksesori yang tersedia untuk produk ini
     addons: [
       {
